37dc4f9183803c2505fcfa7ea496572b6becf6d6
Ahtisham <ahtishamul.haq@macromodule.net>
2026-01-23 13:02:59 +0500

    chore(footer): disable all external links

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -42,37 +42,37 @@ const Footer = () => {
       icon: 'edit' as const,
       iconSize: '16px',
       text: 'Submit an issue',
-      url: issueUrl,
+      url: "#",
     },
     {
       icon: 'social/git' as const,
       iconSize: '18px',
       text: 'Contribute',
-      url: 'https://github.com/blockscout/blockscout',
+       url: "#",
     },
     {
       icon: 'social/twitter' as const,
       iconSize: '18px',
       text: 'X (ex-Twitter)',
-      url: 'https://x.com/blockscout',
+       url: "#",
     },
     {
       icon: 'social/discord' as const,
       iconSize: '24px',
       text: 'Discord',
-      url: 'https://discord.gg/blockscout',
+       url: "#",
     },
     {
       icon: 'brands/blockscout' as const,
       iconSize: '18px',
       text: 'All chains',
-      url: 'https://www.blockscout.com/chains-and-projects',
+       url: "#",
     },
     {
       icon: 'donate' as const,
       iconSize: '20px',
       text: 'Donate',
-      url: 'https://eth.blockscout.com/address/0xfB4aF6A8592041E9BcE186E5aC4BDbd2B137aD11',
+       url: "#",
     },
   ];
 
