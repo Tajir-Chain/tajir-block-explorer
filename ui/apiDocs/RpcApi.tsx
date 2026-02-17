046082214bb06858950bf61e13838df32de8eacd
Ahtisham <ahtishamul.haq@macromodule.net>
2026-02-17 19:37:37 +0500

    Update API documentation links

--- ui/apiDocs/RpcApi.tsx
+++ ui/apiDocs/RpcApi.tsx
@@ -10,8 +10,7 @@ const RpcApi = () => {
         This API is provided for developers transitioning applications from Etherscan to Tajir Explorer API  and applications requiring general API and data support.
         It supports GET and POST requests.
       </Text>
-      {/* <Link href="https://docs.blockscout.com/devs/apis/rpc" external mt={ 6 }>View modules</Link> */}
-      <Link href="" external mt={ 6 }>View modules</Link>
+      <Link href="https://app.gitbook.com/o/w5Q87ZGYNeEKY9GUGNBA/s/AbtkglQlMKahYwDUwHV1/json-rpc-and-eth-compatible-rpc-endpoints" external mt={ 6 }>View modules</Link>
     </Box>
   );
 };
