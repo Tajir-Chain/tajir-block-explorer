835ce34e493227c32a082373a6078675dcb65c1f
Ahtisham <ahtishamul.haq@macromodule.net>
2026-05-04 16:02:56 +0500

    feat(ui): add mobile network info display in HeroBanner with copy functionality

--- ui/home/HeroBanner.tsx
+++ ui/home/HeroBanner.tsx
@@ -1,10 +1,9 @@
 // we use custom heading size for hero banner
 // eslint-disable-next-line no-restricted-imports
-import { Box, Flex, Heading, Text, Grid } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
 
 import config from 'configs/app';
-import useIsMobile from 'lib/hooks/useIsMobile';
 import RewardsButton from 'ui/rewards/RewardsButton';
 // import AdBanner from 'ui/shared/ad/AdBanner';
 import SearchBar from 'ui/snippets/searchBar/SearchBarDesktop';
@@ -20,9 +19,27 @@ export const BACKGROUND_DEFAULT =
 const TEXT_COLOR_DEFAULT = 'white';
 const BORDER_DEFAULT = 'none';
 
-const HeroBanner = () => {
+const copyableHoverStyle = {
+  color: { _light: 'black', _dark: 'white' },
+  _before: {
+    content: `" "`,
+    position: 'absolute' as const,
+    top: '-2px',
+    left: '-8px',
+    width: 'calc(100% + 12px)',
+    height: 'calc(100% + 4px)',
+    borderRadius: 'base',
+    borderColor: 'address.highlighted.border',
+    borderWidth: '1px',
+    borderStyle: 'dashed',
+    bgColor: 'address.highlighted.bg',
+    zIndex: -1,
+  },
+};
 
-  const isMobile = useIsMobile();
+const hasNetworkInfo = Boolean(config.chain.rpcUrls?.[0] || config.chain.id);
+
+const HeroBanner = () => {
 
   const background = {
     _light:
@@ -88,6 +105,77 @@ const HeroBanner = () => {
             </Box>
           ) }
         </Flex>
+
+        { /* Mobile: subtle inline network info below heading */ }
+        { hasNetworkInfo && (
+          <Flex
+            display={{ base: 'flex', lg: 'none' }}
+            alignItems="center"
+            gap={ 3 }
+            mb={ 2 }
+            flexWrap="wrap"
+            bg="blackAlpha.400"
+            backdropFilter="blur(12px)"
+            borderRadius="md"
+            border="1px solid"
+            borderColor="whiteAlpha.200"
+            py={ 1.5 }
+            px={ 3 }
+          >
+            { config.chain.rpcUrls?.[0] && (
+              <Flex
+                alignItems="center"
+                className="address-entity"
+                borderRadius="md"
+                cursor="pointer"
+                position="relative"
+                color="whiteAlpha.800"
+                _hover={ copyableHoverStyle }
+              >
+                <Text fontSize="2xs" fontWeight="medium" color="whiteAlpha.500" mr={ 1 }>
+                  RPC
+                </Text>
+                <Text fontSize="2xs" color="inherit" wordBreak="break-all">
+                  { config.chain.rpcUrls[0] }
+                </Text>
+                <CopyToClipboard
+                  text={ config.chain.rpcUrls[0] }
+                  boxSize={ 3.5 }
+                  ml={ 0.5 }
+                />
+              </Flex>
+            ) }
+
+            { config.chain.rpcUrls?.[0] && config.chain.id && (
+              <Text fontSize="2xs" color="whiteAlpha.400">•</Text>
+            ) }
+
+            { config.chain.id && (
+              <Flex
+                alignItems="center"
+                className="address-entity"
+                borderRadius="md"
+                cursor="pointer"
+                position="relative"
+                color="whiteAlpha.800"
+                _hover={ copyableHoverStyle }
+              >
+                <Text fontSize="2xs" fontWeight="medium" color="whiteAlpha.500" mr={ 1 }>
+                  Chain ID
+                </Text>
+                <Text fontSize="2xs" color="inherit">
+                  { config.chain.id }
+                </Text>
+                <CopyToClipboard
+                  text={ String(config.chain.id) }
+                  boxSize={ 3.5 }
+                  ml={ 0.5 }
+                />
+              </Flex>
+            ) }
+          </Flex>
+        ) }
+
         <Box display={{ base: 'flex', lg: 'none' }}>
           <SearchBarMobile isHeroBanner/>
         </Box>
@@ -95,8 +183,11 @@ const HeroBanner = () => {
           <SearchBar isHeroBanner/>
         </Box>
       </Box>
-      { !isMobile && (
+
+      { /* Desktop: vertical side panel */ }
+      { hasNetworkInfo && (
         <Flex
+          display={{ base: 'none', lg: 'flex' }}
           direction="column"
           justifyContent="center"
           alignItems="flex-start"
@@ -123,23 +214,7 @@ const HeroBanner = () => {
                 cursor="pointer"
                 position="relative"
                 color="white"
-                _hover={{
-                  color: { _light: 'black', _dark: 'white' },
-                  _before: {
-                    content: `" "`,
-                    position: 'absolute',
-                    top: '-2px',
-                    left: '-8px',
-                    width: `calc(100% + 12px)`,
-                    height: 'calc(100% + 4px)',
-                    borderRadius: 'base',
-                    borderColor: 'address.highlighted.border',
-                    borderWidth: '1px',
-                    borderStyle: 'dashed',
-                    bgColor: 'address.highlighted.bg',
-                    zIndex: -1,
-                  }
-                }}
+                _hover={ copyableHoverStyle }
               >
                 <Text fontSize="xs" color="inherit" maxW="260px" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
                   { config.chain.rpcUrls[0] }
@@ -165,23 +240,7 @@ const HeroBanner = () => {
                 cursor="pointer"
                 position="relative"
                 color="white"
-                _hover={{
-                  color: { _light: 'black', _dark: 'white' },
-                  _before: {
-                    content: `" "`,
-                    position: 'absolute',
-                    top: '-2px',
-                    left: '-8px',
-                    width: `calc(100% + 12px)`,
-                    height: 'calc(100% + 4px)',
-                    borderRadius: 'base',
-                    borderColor: 'address.highlighted.border',
-                    borderWidth: '1px',
-                    borderStyle: 'dashed',
-                    bgColor: 'address.highlighted.bg',
-                    zIndex: -1,
-                  }
-                }}
+                _hover={ copyableHoverStyle }
               >
                 <Text fontSize="xs" color="inherit">
                   { config.chain.id }
@@ -201,3 +260,4 @@ const HeroBanner = () => {
 };
 
 export default React.memo(HeroBanner);
+
